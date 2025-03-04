8a9d1a1a2c1be554673b5c70606ef63b56ff23ba
Skycoder42 <Skycoder42@users.noreply.github.com>
2025-03-04 18:35:07 +0100

    configure basic linting and hooks

--- bin/bw_pinentry.dart
+++ bin/bw_pinentry.dart
@@ -1,5 +1,1 @@
-import 'package:bw_pinentry/bw_pinentry.dart' as bw_pinentry;
-
-void main(List<String> arguments) {
-  print('Hello world: ${bw_pinentry.calculate()}!');
-}
+void main(List<String> arguments) {}
--- test/bw_pinentry_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bw_pinentry/bw_pinentry.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
